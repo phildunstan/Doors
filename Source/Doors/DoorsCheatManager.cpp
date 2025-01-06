1995c0e201ff44d1dbfd788800c0259b4855ecdf
Philip Dunstan <phil@philipdunstan.com>
2025-01-06 02:26:44 +0000

    Fixed some door setup issues with examples 1,2, and 3. Added better debug for NavModifierVolumes

---
+++ Source/Doors/DoorsCheatManager.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorsCheatManager.h"
+
+#include "Materials/MaterialParameterCollectionInstance.h"
+
+void UDoorsCheatManager::ShowNavModifierOverrideVolumeDebug()
+{
+    if (NavModifierOverrideVolumeDebugMPC)
+    {
+		if (UMaterialParameterCollectionInstance* MPCInstance = GetWorld()->GetParameterCollectionInstance(NavModifierOverrideVolumeDebugMPC))
+        {
+			MPCInstance->SetScalarParameterValue("ShowNavModifierVolumeDebug", 1);
+        }
+    }
+}
+
+void UDoorsCheatManager::HideNavModifierOverrideVolumeDebug()
+{
+    if (NavModifierOverrideVolumeDebugMPC)
+    {
+		if (UMaterialParameterCollectionInstance* MPCInstance = GetWorld()->GetParameterCollectionInstance(NavModifierOverrideVolumeDebugMPC))
+        {
+			MPCInstance->SetScalarParameterValue("ShowNavModifierVolumeDebug", 0);
+        }
+    }
+}
---
+++ Source/Doors/DoorsCheatManager.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CheatManager.h"
+#include "DoorsCheatManager.generated.h"
+
+class UMaterialParameterCollection;
+
+/**
+ * Custom cheat manager to allow for extra debug actions.
+ */
+UCLASS()
+class DOORS_API UDoorsCheatManager : public UCheatManager
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Doors")
+	TObjectPtr<UMaterialParameterCollection> NavModifierOverrideVolumeDebugMPC;
+	
+	UFUNCTION(exec, BlueprintCallable, Category="Doors")
+	void ShowNavModifierOverrideVolumeDebug();
+
+	UFUNCTION(exec, BlueprintCallable, Category="Doors")
+	void HideNavModifierOverrideVolumeDebug();
+};

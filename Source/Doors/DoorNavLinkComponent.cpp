9a5dca9a860ccb2e1e63cfd7ab9c62014c1579b4
Philip Dunstan <phil@philipdunstan.com>
2025-01-22 00:44:16 +0000

    Fixed the door navmesh modifier not generating navmesh through the doorway by replacing walls with navmesh blocking collision instead of volume modifiers.

--- Source/Doors/DoorNavLinkComponent.cpp
+++ Source/Doors/DoorNavLinkComponent.cpp
@@ -3,6 +3,30 @@
 
 #include "DoorNavLinkComponent.h"
 
+#include "NavigationSystem.h"
+
+
+void RebuildNavMeshCommand()
+{
+	UWorld* World = GEngine->GetWorldContexts()[0].World();
+	if (!World)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No valid world context!"));
+		return;
+	}
+
+	if (UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(World))
+	{
+		NavSys->Build();
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("NavMesh regeneration triggered!"));
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Failed to find Navigation System!"));
+	}
+}
+FAutoConsoleCommand RebuildNavMeshCmd(TEXT("RebuildNavMesh"), TEXT("Forces NavMesh regeneration."), FConsoleCommandDelegate::CreateStatic(RebuildNavMeshCommand));
+
 
 UDoorNavLinkComponent::UDoorNavLinkComponent()
 {
